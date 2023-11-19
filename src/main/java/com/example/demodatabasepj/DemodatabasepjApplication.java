116a01e2c37aae4754471df66266e2afbe185261
aliciagarnier <aliciabatistafranco@gmail.com>
2023-11-19 12:06:56 -0300

    refactor: added packages

--- src/main/java/com/example/demodatabasepj/DemodatabasepjApplication.java
+++ src/main/java/com/example/demodatabasepj/DemodatabasepjApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemodatabasepjApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemodatabasepjApplication.class, args);
     }
-
 }
